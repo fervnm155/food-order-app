a34c69fd0e39947cf1198c1791bf49d359bb6a3b
Fernando Sustaita <fernandojsustaita@gmail.com>
2022-11-03 16:10:23 -0600

    app structure with the meal list

---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+import AvailableMeals from "./AvailableMeals";
+
+import MealsSumary from './MealsSummary';
+
+const Meals=()=>{
+    return(
+        <Fragment>
+            <MealsSumary />
+            <AvailableMeals />
+        </Fragment>
+    );
+};
+
+export default Meals;
